ce5aaa3c53b6f6a0425ecb032c236e3c5abf6f8c
Novar <xuwab77@gmail.com>
2023-06-24 23:05:15 +0800

    feat: delete test demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
 import React from 'react'
-// import List1 from './List1'
-// import StateDemo1 from './StateDemo1'
-// import StateDemo2 from './StateDemo2'
-import List2 from './List2'
-// import ImmerDemo from './ImmerDemo'
-// import UseRefDemo from './UseRefDemo'
-// import UseMemoDemo from './UseMemoDemo'
-// import UseCallBackDemo from './UseCallBackDemo'
-// import useTitle from './hooks/UseTitle'
-// import useMouse from './hooks/useMouse'
-// import useGetInfo from './hooks/useGetInfo'
-// import { useTitle } from 'ahooks'
-// import { useMouse } from 'ahooks'
-// import ClosureTrapDemo from './ClosureTrapDemo'
-
 function App() {
-  // useTitle('App-2')
-  // const { x, y } = useMouse()
-  // console.log('Mouse position: ', x, y)
-  // const { loading, info } = useGetInfo()
-  // console.log('loading: ', loading)
-  // console.log('info: ', info)
-  // const mouse = useMouse()
-  // console.log('mouse: ', mouse)
-
   return (
     <>
-      {/* <List1 /> */}
-      {/* <StateDemo1 /> */}
-      {/* <StateDemo2 /> */}
-      <List2 />
-      {/* <ImmerDemo /> */}
-      {/* <UseRefDemo /> */}
-      {/* <UseMemoDemo /> */}
-      {/* <UseCallBackDemo /> */}
-      {/* <ClosureTrapDemo /> */}
+      <div>App</div>
     </>
   )
 }
